35d551a24d66b86dffbac8a770ad643dfa4be38e
hurihuchi <gmldnr96@gmail.com>
2024-01-29 14:22:34 +0900

    apply debounce to editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,17 +1,19 @@
 import { ChangeEvent } from 'react'
+import { useDebounce } from '@toss/react'
 
 interface Props {
   iframeRef: React.RefObject<HTMLIFrameElement>
 }
 
 const Editor = ({ iframeRef }: Props) => {
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+  const handleChange = useDebounce((e: ChangeEvent<HTMLTextAreaElement>) => {
     if (iframeRef?.current) {
       const value = e.target.value
       const html = { type: 'html', value }
       iframeRef.current.contentWindow?.postMessage(html, '*')
     }
-  }
+  }, 500)
+
   return (
     <div className='flex-1 border-r border-stone-600'>
       <textarea
