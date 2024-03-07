bcaf99ebf716422bdaafcd2186ad59b481270259
hurihuchi <gmldnr96@gmail.com>
2024-03-07 14:05:03 +0900

    add whitespce-pre-wrap attr to Source Component

--- src/components/Soruce.tsx
+++ src/components/Soruce.tsx
@@ -4,10 +4,10 @@ import Badge from './ui/Badge'
 const Soruce = () => {
   const sourceCode = useEditorStore((s) => s.sourceCode)
   return (
-    <div className='flex-1 p-4'>
+    <section className='flex-1 p-4'>
       <Badge>Source</Badge>
-      <pre>{sourceCode}</pre>
-    </div>
+      <pre className='whitespace-pre-wrap'>{sourceCode}</pre>
+    </section>
   )
 }
 
