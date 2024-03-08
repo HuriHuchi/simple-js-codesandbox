b6c002a015f39f0f8c084fe9d9066fe3206159df
hurihuchi <gmldnr96@gmail.com>
2024-03-08 13:32:05 +0900

    feat: resize mode

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,27 @@ import { useEditorStore } from './store'
 
 function App() {
   const iframeRef = useRef<HTMLIFrameElement>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const [resizeMode, setResizeMode] = useState(false)
+
   const [errorMessage, setErrorMessage] = useState('')
   const { updateEditorState } = useEditorStore((s) => s.actions)
+
+  useEffect(() => {
+    const handleResize = (e) => {
+      if (containerRef?.current) {
+        containerRef.current.style.width = `${e.clientX}px`
+      }
+    }
+
+    if (resizeMode) {
+      document.addEventListener('mousemove', handleResize)
+      return () => {
+        document.removeEventListener('mousemove', handleResize)
+      }
+    }
+  }, [resizeMode])
 
   useEffect(() => {
     const handleError = ({ error }: ErrorEvent) => {
@@ -19,8 +38,12 @@ function App() {
   }, [updateEditorState])
 
   return (
-    <div className='w-full min-h-dvh flex bg-stone-800'>
-      <Editor iframeRef={iframeRef} />
+    <div className='min-h-dvh flex bg-stone-800'>
+      <Editor ref={containerRef} iframeRef={iframeRef} />
+      <div
+        className='cursor-col-resize h-screen w-2 bg-stone-700 hover:bg-stone-600 transition'
+        onMouseDown={() => setResizeMode(true)}
+        onMouseUp={() => setResizeMode(false)}></div>
       <div className='flex-1 flex flex-col'>
         <Output ref={iframeRef} errorMessage={errorMessage} />
         <Soruce />
